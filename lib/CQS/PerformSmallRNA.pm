02ef89fd5b725ee9f8143f823a2519af8473fd7f
Shilin Zhao <zhaoshilin@gmail.com>
2016-02-09 23:20:53 -0600

    rabbit

--- lib/CQS/PerformSmallRNA.pm
+++ lib/CQS/PerformSmallRNA.pm
@@ -13,7 +13,7 @@ our @ISA = qw(Exporter);
 our %EXPORT_TAGS = (
   'all' => [
     qw(hg19_genome hg19_3utr hg38_genome mm10_genome mm10_3utr rn5_genome cel235_genome cfa3_genome performSmallRNA_hg19 performSmallRNATask_hg19 performSmallRNA_hg20 performSmallRNATask_hg20 performSmallRNA_mm10 performSmallRNATask_mm10 performSmallRNA_rn5 performSmallRNATask_rn5 
-    performSmallRNA_cel235 performSmallRNATask_cel235 performSmallRNA_cfa3 performSmallRNATask_cfa3 performSmallRNA_bta8 performSmallRNATask_bta8 performSmallRNA_eca2 performSmallRNATask_eca2 performSmallRNA_ssc3 performSmallRNATask_ssc3)
+    performSmallRNA_cel235 performSmallRNATask_cel235 performSmallRNA_cfa3 performSmallRNATask_cfa3 performSmallRNA_bta8 performSmallRNATask_bta8 performSmallRNA_eca2 performSmallRNATask_eca2 performSmallRNA_ssc3 performSmallRNATask_ssc3 performSmallRNA_ocu2 performSmallRNATask_ocu2)
   ]
 );
 
@@ -224,6 +224,24 @@ sub ssc3_genome {
   );
 }
 
+sub ocu2_genome {
+  return merge(
+    supplement_genome(),
+    {
+      #genome database
+      mirbase_count_option  => "-p ocu",
+      coordinate            => "/gpfs21/scratch/cqs/zhaos/vickers/reference/Oryctolagus_cuniculus/ocu2_miRBase21_ucsc-tRNA_ensembl83.bed",
+      coordinate_fasta      => "/gpfs21/scratch/cqs/zhaos/vickers/reference/Oryctolagus_cuniculus/ocu2_miRBase21_ucsc-tRNA_ensembl83.bed.fa",
+      
+      bowtie1_index         => "/gpfs21/scratch/cqs/zhaos/vickers/reference/Oryctolagus_cuniculus/bowtie_index_1.1.2/oryCun2",
+#      gsnap_index_directory => "/scratch/cqs/zhaos/vickers/reference/Equus_caballus/gsnap_index_k14_2015-06-23",
+#      gsnap_index_name      => "equCab2",
+      
+#      star_index_directory  => "/scratch/cqs/shengq1/references/cel235/STAR_index_v78_2.4.2a_sjdb49"
+    }
+  );
+}
+
 sub performSmallRNA_hg19 {
   my ( $userdef, $perform ) = @_;
   my $def = getSmallRNADefinition( $userdef, hg19_genome() );
@@ -355,6 +373,21 @@ sub performSmallRNA_ssc3 {
 sub performSmallRNATask_ssc3 {
   my ( $userdef, $task ) = @_;
   my $def = getSmallRNADefinition( $userdef, ssc3_genome() );
+
+  performSmallRNATask( $def, $task );
+}
+
+sub performSmallRNA_ocu2 {
+  my ( $userdef, $perform ) = @_;
+  my $def = getSmallRNADefinition( $userdef, ocu2_genome() );
+
+  my $config = performSmallRNA( $def, $perform );
+  return $config;
+}
+
+sub performSmallRNATask_ocu2 {
+  my ( $userdef, $task ) = @_;
+  my $def = getSmallRNADefinition( $userdef, ocu2_genome() );
 
   performSmallRNATask( $def, $task );
 }
--- lib/CQS/SmallRNA.pm
+++ lib/CQS/SmallRNA.pm
@@ -11,7 +11,7 @@ our %EXPORT_TAGS = (
   'all' => [
     qw(performSmallRNA_hg19 performSmallRNATask_hg19 performSmallRNA_hg20 performSmallRNATask_hg20 performSmallRNA_mm10 performSmallRNATask_mm10 performSmallRNA_rn5 performSmallRNATask_rn5
      performSmallRNA_cel235 performSmallRNATask_cel235 performSmallRNA_cfa3 performSmallRNATask_cfa3 performSmallRNA_bta8 performSmallRNATask_bta8 performSmallRNA_eca2 performSmallRNATask_eca2
-      performSmallRNA_ssc3 performSmallRNATask_ssc3)
+      performSmallRNA_ssc3 performSmallRNATask_ssc3 performSmallRNA_ocu2 performSmallRNATask_ocu2)
   ]
 );
 
@@ -89,6 +89,14 @@ sub performSmallRNA_ssc3 {
 
 sub performSmallRNATask_ssc3 {
   CQS::PerformSmallRNA::performSmallRNATask_ssc3(@_);
+}
+
+sub performSmallRNA_ocu2 {
+  CQS::PerformSmallRNA::performSmallRNA_ocu2(@_);
+}
+
+sub performSmallRNATask_ocu2 {
+  CQS::PerformSmallRNA::performSmallRNATask_ocu2(@_);
 }
 
 1;
