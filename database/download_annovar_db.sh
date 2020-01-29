70e9e47010c30cfc8824d07dffa6b8a4bb1a19fc
Quanhu Sheng <shengqh@gmail.com>
2020-01-29 10:53:42 -0600

    download db script

---
+++ database/download_annovar_db.sh
@@ -0,0 +1,8 @@
+annotate_variation.pl -downdb -webfrom annovar -buildver hg38 avsnp150 .
+annotate_variation.pl -downdb -webfrom annovar -buildver hg19 1000g2015aug .
+annotate_variation.pl -downdb -webfrom annovar -buildver hg19 clinvar_20180603 .
+annotate_variation.pl -downdb -webfrom annovar -buildver hg38 clinvar_20180603 .
+annotate_variation.pl -downdb -webfrom annovar -buildver hg19 exac03nontcga .
+annotate_variation.pl -downdb -webfrom annovar -buildver hg19 esp6500siv2 .
+annotate_variation.pl -downdb -buildver hg19 gwasCatalog .
+annotate_variation.pl -downdb -buildver hg38 gwasCatalog .
