e0988d13f2dfef66d834094ab45af00787cc4b72
Quanhu Sheng <shengqh@gmail.com>
2020-01-29 12:15:16 -0600

    annovar db

--- database/download_annovar_db.sh
+++ database/download_annovar_db.sh
@@ -2,7 +2,7 @@ cd /scratch/cqs_share/references/annovar/humandb
 
 for hg in hg19 hg38
 do
-  for db in refGene avsnp150 1000g2015aug gnomad211_genome clinvar_20190305 exac03nontcga esp6500siv2 gwasCatalog
+  for db in refGene avsnp150 1000g2015aug gnomad211_genome clinvar_20190305 exac03 esp6500siv2 gwasCatalog
   do
     annotate_variation.pl -downdb -webfrom annovar -buildver $hg $db .
   done
