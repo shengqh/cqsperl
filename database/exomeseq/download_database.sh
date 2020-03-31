ad0a7b1a4303ab4397376c60f8e38991e3b82ea7
Quanhu Sheng <shengqh@gmail.com>
2020-03-31 14:44:03 -0500

    rename and download dbsnp

--- database/exomeseq/download_database.sh
+++ database/exomeseq/download_database.sh
@@ -1,3 +1,5 @@
+##download capture bed
+
 mkdir /scratch/cqs_share/references/exomeseq
 
 mkdir /scratch/cqs_share/references/exomeseq/IDT
@@ -10,4 +12,13 @@ cd /scratch/cqs_share/references/exomeseq/Twist
 cut -f 2,3 ~/program/cqsperl/database/exomeseq/Homo_sapiens_assembly38.dict | sed "s/SN://g" |sed "s/LN://g" | grep -v "VN:" | grep -v "_" | grep -v "HLA" | grep -v "chrEBV" > hg38.genome
 wget https://www.twistbioscience.com/sites/default/files/resources/2019-06/Twist_Exome_Target_hg38.bed
 bedtools slop -i Twist_Exome_Target_hg38.bed -g hg38.genome -b 50 > Twist_Exome_Target_hg38.slop50.bed
+
+##download dbsnp for mm10
+
+cd /scratch/cqs_share/references/dbsnp
+wget https://ftp.ncbi.nih.gov/snp/pre_build152/organisms/archive/mouse_10090/VCF/00-All.vcf.gz 
+zcat 00-All.vcf.gz | awk '{if($0 !~ /^#/) print "chr"$0; else print $0}' | gzip > mouse_10090_b150_GRCm38.p4.vcf.gz
+tabix -p vcf mouse_10090_b150_GRCm38.p4.vcf.gz
+rm 00-All.vcf.gz
+
 
