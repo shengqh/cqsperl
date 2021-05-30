3ddae6a59f10a0e1939a2de78306005ac8ee20a4
Quanhu Sheng <shengqh@gmail.com>
2021-05-30 17:48:13 -0500

    remove tmp_dir

--- singularity/build_scrnaseq.sh
+++ singularity/build_scrnaseq.sh
@@ -1,2 +1,2 @@
-singularity build --disable-cache --tmpdir . cqs-scrnaseq.20210426.simg docker://shengqh/cqs_scrnaseq:20210426
+singularity build --disable-cache cqs-scrnaseq.20210528.simg docker://shengqh/cqs_scrnaseq:20210528
 
