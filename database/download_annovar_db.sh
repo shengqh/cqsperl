dccd00c1e0b6562ad111f6d2a7c66ecccf96fa6b
Quanhu Sheng <shengqh@gmail.com>
2021-07-05 14:48:11 -0500

    download cosmic70

--- database/download_annovar_db.sh
+++ database/download_annovar_db.sh
@@ -2,7 +2,7 @@ cd /data/cqs/references/annovar/humandb
 
 for hg in hg19 hg38
 do
-  for db in refGene avsnp150 1000g2015aug gnomad211_genome clinvar_20190305 exac03 esp6500siv2 gwasCatalog
+  for db in refGene avsnp150 1000g2015aug gnomad211_genome clinvar_20190305 exac03 esp6500siv2 gwasCatalog cosmic70
   do
     annotate_variation.pl -downdb -webfrom annovar -buildver $hg $db .
   done
