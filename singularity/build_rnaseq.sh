5cf7fafcdd2d1225e6360ba0b5665a0a73600dfd
Quanhu Sheng <shengqh@gmail.com>
2021-07-23 21:12:39 -0500

    v20210723

--- singularity/build_rnaseq.sh
+++ singularity/build_rnaseq.sh
@@ -1,2 +1,2 @@
-singularity build --disable-cache --tmpdir . cqs-rnaseq.20210413.simg docker://shengqh/cqs_rnaseq:20210413
+singularity build --disable-cache cqs-rnaseq.20210723.simg docker://shengqh/cqs_rnaseq:20210723
 
