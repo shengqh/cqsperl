7bf79ca2a5c9584a516d22986746b4b6cdb51463
Quanhu Sheng <shengqh@gmail.com>
2022-02-01 08:00:40 -0600

    v20220201

--- singularity/build_bamsnap.sh
+++ singularity/build_bamsnap.sh
@@ -1,3 +1,3 @@
 #singularity build --disable-cache --tmpdir . bamsnap.simg docker://danielmsk/bamsnap
-singularity build --disable-cache cqs-bamsnap.20210826.simg docker://shengqh/cqs_bamsnap:20210826
+singularity build --disable-cache cqs-bamsnap.20220121.sif docker://shengqh/cqs_bamsnap:20220121
 
