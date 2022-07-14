7b5d62ebf07bffdb31760614283cb5ddf392dcce
Quanhu Sheng <shengqh@gmail.com>
2022-07-14 02:04:07 -0500

    v20220714

---
+++ singularity/build_gotc.sh
@@ -0,0 +1,2 @@
+singularity build genomes-in-the-cloud.2.4.7-1603303710.sif docker://us.gcr.io/broad-gotc-prod/genomes-in-the-cloud:2.4.7-1603303710
+
---
+++ singularity/build_immunarch.sh
@@ -0,0 +1,2 @@
+singularity build --disable-cache immunarch.20220711.sif docker://shengqh/immunarch:20220711
+
