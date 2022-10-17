9159cb0b605f748e0a4d68666fcc5c7302c420b8
Quanhu Sheng <shengqh@gmail.com>
2022-10-17 11:40:22 -0500

    v20221017

---
+++ singularity/build_gistic2.sh
@@ -0,0 +1,3 @@
+#singularity build --disable-cache gistic2_23.sif docker://genepattern/docker-gistic
+singularity build --disable-cache gistic2.sif docker://shixiangwang/gistic
+
--- singularity/build_scrnaseq.sh
+++ singularity/build_scrnaseq.sh
@@ -1,2 +1,2 @@
-singularity build --disable-cache cqs-scrnaseq.20220603.sif docker://shengqh/cqs_scrnaseq:20220603
+singularity build --disable-cache cqs-scrnaseq.20221011.sif docker://shengqh/cqs_scrnaseq:20221011
 
