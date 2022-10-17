684e7a279dcf21257e0e081e97796fc8d36220f3
Quanhu Sheng <shengqh@gmail.com>
2022-10-17 11:44:18 -0500

    bugfix

--- lib/CQS/PerformExomeSeq.pm
+++ lib/CQS/PerformExomeSeq.pm
@@ -68,9 +68,9 @@ sub global_definition {
       #"cromwell_jar" => "/data/cqs/softwares/cromwell/cromwell-53.1.jar",
       #"cromwell_jar" => "/data/cqs/softwares/cromwell/cromwell-64.jar",
       "cromwell_jar" => "/data/cqs/softwares/cromwell/cromwell-81.jar",
-      "cromwell_option_file" => "/data/cqs/cqsperl/config/wdl/cromwell.options.json",
+      "cromwell_option_file" => "/data/cqs/softwares/cqsperl/config/wdl/cromwell.options.json",
       "local" => {
-        "cromwell_config_file" => "/data/cqs/cqsperl/config/wdl/cromwell.local.nopull.conf",
+        "cromwell_config_file" => "/data/cqs/softwares/cqsperl/config/wdl/cromwell.local.nopull.conf",
         #"cromwell_config_file" => "/home/zhaos/source/perl_cqs/test/cromwell/cromwell.examples.local.conf",
         "mutect2" => {
           "perform_mutect2_pon" => 0,
