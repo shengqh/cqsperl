28b6b628ea31e201ea1a438682f70d335ebca7a6
Quanhu Sheng <shengqh@gmail.com>
2022-12-29 16:03:18 -0600

    update singularity

---
+++ singularity/build_rstudio.sh
@@ -0,0 +1,2 @@
+singularity build --disable-cache rstudio.4.2.2.sif docker://rocker/rstudio:4.2.2
+
--- singularity/build_scrnaseq.sh
+++ singularity/build_scrnaseq.sh
@@ -1,2 +1,2 @@
-singularity build --disable-cache cqs-scrnaseq.20221116.sif docker://shengqh/cqs_scrnaseq:20221116
+singularity build --disable-cache cqs-scrnaseq.20221223.sif docker://shengqh/cqs_scrnaseq:20221223
 
