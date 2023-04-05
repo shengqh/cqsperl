00701c342b49b04140939ad118bcb9f52e7d18b4
Quanhu Sheng <shengqh@gmail.com>
2023-04-05 11:34:20 -0500

    remove SINGULARITY_CACHEDIR

--- scripts/set_singularity_cachedir.sh
+++ scripts/set_singularity_cachedir.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export SINGULARITY_CACHEDIR=/data/cqs/softwares/singularity/cache
+#export SINGULARITY_CACHEDIR=/data/cqs/softwares/singularity/cache
 export SINGULARITY_TMPDIR=/data/cqs/softwares/singularity/tmp
 export SINGULARITY_PULLDIR=/data/cqs/softwares/singularity
 export CWL_SINGULARITY_CACHE=$SINGULARITY_PULLDIR
