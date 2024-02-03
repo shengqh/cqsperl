8d1a65a3a539e5214ca252b60b5c7712e59f5573
Quanhu Sheng <shengqh@gmail.com>
2024-02-02 23:47:36 -0600

    new spcount for smallrna

--- singularity/build_smallrnaseq.sh
+++ singularity/build_smallrnaseq.sh
@@ -1,2 +1,2 @@
-singularity build --disable-cache cqs_smallrnaseq.20240117.sif docker://shengqh/cqs_smallrnaseq:20240117
+singularity build --disable-cache cqs_smallrnaseq.20240201.sif docker://shengqh/cqs_smallrnaseq:20240201
 
