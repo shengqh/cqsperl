605d587e83389e0922be1773a38eb928b08b6470
Quanhu Sheng <shengqh@gmail.com>
2024-03-20 13:57:43 -0500

    singularity

--- singularity/build_chipseq.sh
+++ singularity/build_chipseq.sh
@@ -1,2 +1,2 @@
-singularity build cqs-chipseq.20231017.sif docker://shengqh/cqs_chipseq:20231017
+singularity build cqs-chipseq.20240318.sif docker://shengqh/cqs_chipseq:20240318
 
---
+++ singularity/build_scaden.sh
@@ -0,0 +1,2 @@
+singularity build --disable-cache scaden.v1.1.2.sif docker://deconvolution/scaden:latest
+
