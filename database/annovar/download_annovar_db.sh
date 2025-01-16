01424dd246982cf486e1fe6739e6d14c80e9ea15
Quanhu Sheng <shengqh@gmail.com>
2025-01-16 14:17:48 -0600

    download annovar db

---
+++ database/annovar/download_annovar_db.sh
@@ -0,0 +1,16 @@
+cd /data/cqs/references/annovar/humandb
+
+for hg in hg19 hg38
+do
+  for db in refGene avsnp150 1000g2015aug gnomad41_genome clinvar_20190305 exac03 esp6500siv2 gwasCatalog cosmic70 cytoBand 
+  do
+    annotate_variation.pl -downdb -webfrom annovar -buildver $hg $db .
+  done
+done
+
+cd /data/cqs/references/annovar/mousedb
+
+annotate_variation.pl -downdb -webfrom annovar -buildver mm10 refGene .
+annotate_variation.pl --buildver mm10 --downdb seq ./mm10_seq
+retrieve_seq_from_fasta.pl ./mm10_refGene.txt -seqdir ./mm10_seq -format refGene -outfile ./mm10_refGeneMrna.fa
+rm -rf mm10_seq
