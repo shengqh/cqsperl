5776046122afaf6376e253cc4d030045e802da09
Quanhu Sheng <shengqh@gmail.com>
2025-02-03 00:46:33 -0600

    R 441

--- singularity/build_cqs_bioconductor.sh
+++ singularity/build_cqs_bioconductor.sh
@@ -1,2 +1,2 @@
-singularity build --disable-cache cqs_bioconductor.3_19_R_4_4_0.20240510.sif docker://shengqh/cqs_bioconductor_3_19:20240510
-
+#singularity build --disable-cache cqs_bioconductor.3_19_R_4_4_0.20240510.sif docker://shengqh/cqs_bioconductor_3_19:20240510
+singularity build --disable-cache cqs_bioconductor.3_19_R_4_4_1.20250122.sif docker://shengqh/cqs_bioconductor_3_19:20250122
