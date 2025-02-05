348f6ac2dc26002f1ec2718f0bf33884572dc062
Quanhu Sheng <shengqh@gmail.com>
2025-02-05 14:30:57 -0600

    scrnaseq

--- singularity/build_scrnaseq.sh
+++ singularity/build_scrnaseq.sh
@@ -1,2 +1,2 @@
-singularity build --disable-cache cqs-scrnaseq.20241113.sif docker://shengqh/cqs_scrnaseq:20241113
+singularity build --disable-cache cqs-scrnaseq.20241229.sif docker://shengqh/cqs_scrnaseq:20241229
 
