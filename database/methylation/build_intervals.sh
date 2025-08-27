9bc5eab89c26a20461b3801ab5da400c25d26f6f
Quanhu Sheng <shengqh@gmail.com>
2025-08-27 16:54:04 -0500

    build methylation interval

---
+++ database/methylation/build_intervals.sh
@@ -0,0 +1,6 @@
+cd /data/cqs/references/methylation/twist
+
+gatk BedToIntervalList \
+    -I covered_targets_Twist_Methylome_hg38_annotated_collapsed.bed \
+    -O covered_targets_Twist_Methylome_hg38_annotated_collapsed.intervals \
+    -SD /data/cqs/references/gencode/GRCh38.p13/GRCh38.primary_assembly.genome.dict
