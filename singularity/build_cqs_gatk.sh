681aa7566f805d3b3f0e16fbc7368ab508f282a4
Quanhu Sheng <shengqh@gmail.com>
2025-10-08 21:11:17 -0500

    build cqs gatk to 4.6.2.0

--- singularity/build_cqs_gatk.sh
+++ singularity/build_cqs_gatk.sh
@@ -1,2 +1,2 @@
-singularity build --disable-cache cqs-gatk-4.5.0.0.20240216.sif docker://shengqh/cqs_gatk4.5.0.0:20240216
+singularity build --disable-cache cqs-gatk-4.6.2.0.20251008.sif docker://shengqh/cqs_gatk4.6.2.0:20251008
 
