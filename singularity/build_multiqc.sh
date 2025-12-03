e87098f04b2ed4c2d3df96beb67f520319d663d6
Quanhu Sheng <shengqh@gmail.com>
2025-12-02 23:53:55 -0600

    v20251102

--- singularity/build_multiqc.sh
+++ singularity/build_multiqc.sh
@@ -1,1 +1,3 @@
-singularity build --disable-cache multiqc.v1.31.sif docker://multiqc/multiqc:v1.31
+cd /data/cqs/softwares/singularity
+
+singularity build --disable-cache multiqc.v1.32.sif docker://multiqc/multiqc:v1.32
