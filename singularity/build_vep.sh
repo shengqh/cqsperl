c37f53ffcc8e203d942481e58f85a9ee3e4baf4e
Quanhu Sheng <shengqh@gmail.com>
2026-03-12 00:04:36 -0500

    vep 115

--- singularity/build_vep.sh
+++ singularity/build_vep.sh
@@ -1,2 +1,4 @@
-singularity build --disable-cache ensembl-vep.104.3.simg docker://ensemblorg/ensembl-vep:release_104.3
+cd /data/cqs/softwares/singularity
+
+singularity build --disable-cache ensembl-vep.115.2.sif docker://ensemblorg/ensembl-vep:release_115.2
 
