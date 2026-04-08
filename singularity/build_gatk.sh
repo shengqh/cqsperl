3f72892d94f4dbd96990729d56cd4e91db53291f
Quanhu Sheng <shengqh@gmail.com>
2026-04-08 12:43:22 -0500

    build 4.6.1.0

--- singularity/build_gatk.sh
+++ singularity/build_gatk.sh
@@ -1,2 +1,3 @@
-singularity build gatk.4.6.2.0.sif docker://broadinstitute/gatk:4.6.2.0
+singularity build gatk.4.6.1.0.sif docker://broadinstitute/gatk:4.6.1.0
+#singularity build gatk.4.6.2.0.sif docker://broadinstitute/gatk:4.6.2.0
 
