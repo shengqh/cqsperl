4fc9c0c3ca9cd2ccb94de60357bdee92fc99ae51
Quanhu Sheng <shengqh@gmail.com>
2026-05-13 00:03:33 -0500

    celltypist and stcat images

---
+++ singularity/build_celltypist.sh
@@ -0,0 +1,4 @@
+cd /data/cqs/softwares/singularity
+
+singularity build --disable-cache celltypist.20260512.sif docker://shengqh/celltypist:20260512
+
---
+++ singularity/build_stcat.sh
@@ -0,0 +1,4 @@
+cd /data/cqs/softwares/singularity
+
+singularity build --disable-cache stcat.20260512.sif docker://shengqh/stcat:20260512
+
