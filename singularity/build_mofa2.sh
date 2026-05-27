e96da713c44ca96d2673d2bd711621a08ee018de
Quanhu Sheng <shengqh@gmail.com>
2026-05-27 13:52:46 -0500

    update singularity

---
+++ singularity/build_mofa2.sh
@@ -0,0 +1,4 @@
+cd /data/cqs/softwares/singularity
+
+singularity build --disable-cache mofa2.20260525.sif docker://shengqh/mofa2:20260525
+
--- singularity/build_plink.sh
+++ singularity/build_plink.sh
@@ -1,1 +1,3 @@
-singularity build --disable-cache plink_1.9_2.0.20250620.sif docker://shengqh/plink_1.9_2.0:20250620
+cd /data/cqs/softwares/singularity
+
+singularity build --disable-cache plink_1.9_2.0.20260526.sif docker://shengqh/plink_1.9_2.0:20260526
