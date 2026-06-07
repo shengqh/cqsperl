54bb5a2d2318cb52f5da03129f853d079bc6b5a4
Quanhu Sheng <shengqh@gmail.com>
2026-06-07 11:24:18 -0500

    new autogvp

--- singularity/build_autogvp.sh
+++ singularity/build_autogvp.sh
@@ -1,4 +1,4 @@
 cd /data/cqs/softwares/singularity
 
-singularity build --disable-cache autogvp.v1.0.5.sif docker://pgc-images.sbgenomics.com/diskin-lab/autogvp:v1.0.5
+singularity build --disable-cache autogvp.v1.0.5.sif docker://shengqh/autogvp:v1.0.5
 
