7149b6be48749b90144d1327fbdd5e330215799d
Quanhu Sheng <shengqh@gmail.com>
2026-09-21 12:53:01 -0500

    new scrna image

--- singularity/build_scrnaseq.sh
+++ singularity/build_scrnaseq.sh
@@ -1,4 +1,4 @@
 cd /data/cqs/softwares/singularity
 
-singularity build --disable-cache cqs-scrnaseq.20260516.sif docker://shengqh/cqs_scrnaseq:20260516
+singularity build --disable-cache cqs-scrnaseq.20260921.sif docker://shengqh/cqs_scrnaseq:20260921
 
