5d282877991f90e7b111da5ee63c1ddf0ca83ab1
Quanhu Sheng <shengqh@gmail.com>
2026-09-25 12:28:00 -0500

    upgrade image

--- singularity/build_wgbs.sh
+++ singularity/build_wgbs.sh
@@ -1,4 +1,4 @@
 cd /data/cqs/softwares/singularity
 
-singularity build --disable-cache cqs_wgbs.20260923.sif docker://shengqh/cqs_wgbs:20260923
+singularity build --disable-cache cqs_wgbs.20260925.sif docker://shengqh/cqs_wgbs:20260925
 
